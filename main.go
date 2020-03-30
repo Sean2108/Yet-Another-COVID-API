05eb0ca0f6a2bb8ec7b15dccf7ccbfc923f8226e
Sean2108 <sean2108@gmail.com>
2020-03-31 00:05:30 +0100

    missing : in port number

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ func setupRoutes() {
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = ":8080"
+		port = "8080"
 	}
 	// the John Hopkins data is updated at 11:50 or 11:55 pm GMT everyday, so we will call update at midnight utc (0 hour)
 	schedule.CallFunctionDaily(casecount.UpdateCaseCounts, 0)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	setupRoutes()
-	log.Printf("Server started at port%s", port)
-	log.Fatalln(http.ListenAndServe(port, nil))
+	log.Printf("Server started at port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 	wg.Wait()
 }
